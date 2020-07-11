ca8b4be527670d9db42597287befdbaf4609bb3c
marco <tanzi.marco@gmail.com>
2020-07-11 15:33:21 +0200

    Add github merge

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const github = require('@actions/github')
+const core = require('@actions/core');
+const { Octokit } = require('@octokit/rest')
+
+const job = JSON.parse(core.getInput('job'));
+const token = core.getInput('github_token')
+const octokit = new Octokit({ auth: token })
+
+const repo = github.context.repo
+
+async function merge(source, target) {
+  core.info(`merge branch:${source} to: ${target}`)
+
+  const response = await octokit.repos.merge({
+    owner: repo.owner,
+    repo: repo.repo,
+    base: target,
+    head: source,
+    commit_message: `Merged '${source}' into '${target}'.`
+  })
+}
+
+async function run() {
+  const source = core.getInput('source')
+  const target = core.getInput('target')
+  core.info(`merge ${source} into ${target}`)
+
+  try {
+    await merge(source, target)
+  } catch (error) {
+    core.setFailed(`${source} merge failed: ${error.message}`)
+  }
+
+}
+
+run()
